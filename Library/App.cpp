a881b3418fd081b04d0e570b081d579d6d34c044
saijou <>
2025-12-10 14:36:11 +0900

    ライブラリ追加

---
+++ Library/App.cpp
@@ -0,0 +1,47 @@
+#include "App.h"
+#include "SceneManager.h"
+#include "ObjectManager.h"
+#include "Time.h"
+#include "../Source/CSVManager.h"
+#include "../Source/EquipmentsCSVManager.h"
+#include <DxLib.h>
+#include "../Source/ResultCSVManager.h"
+#include "../Source/ButtonPosCSVManager.h"
+#include "../Source/OrdersCSVManager.h"
+
+void AppInit()
+{
+	ObjectManager::Init();
+	SceneManager::Init();
+	Time::Init();
+	CSVManager::LoadSceneTransitions("SceneManager.csv");
+	EquipmentCSVManager::LoadEquipmentsCSV("Equipments.csv");
+	ResultCSVManager::LoadResultCSV("Result.csv");
+	ButtonPosCSVManager::LoadPositionsCSV("ButtonPosition.csv");
+	OrdersCSVManager::LoadOrdersCSV("Orders.csv");
+}
+
+void AppUpdate()
+{
+	SceneManager::Update();
+	ObjectManager::Update();
+}
+
+void AppDraw()
+{
+	Time::Refresh();
+	ObjectManager::Draw();
+	SceneManager::Draw();
+}
+
+void AppRelease()
+{
+	Time::Release();
+	SceneManager::Release();
+	ObjectManager::Release();
+}
+
+bool AppIsExit()
+{
+	return SceneManager::IsExit();
+}
